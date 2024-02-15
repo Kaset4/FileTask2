3f50feb45920bcf8b1e1df84e983f82150d41007
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-02-15 21:01:31 +0400

    Add files via upload

---
+++ FileTask2/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к директории:");
+        string folderPath = Console.ReadLine();
+
+        try
+        {
+            if (Directory.Exists(folderPath))
+            {
+                long folderSize = GetFolderSize(folderPath);
+                Console.WriteLine($"Размер папки {folderPath}: {folderSize} байт");
+            }
+            else
+            {
+                Console.WriteLine("Указанная директория не существует.");
+            }
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine($"Ошибка доступа: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    static long GetFolderSize(string folderPath)
+    {
+        long size = 0;
+
+        foreach (string file in Directory.GetFiles(folderPath))
+        {
+            size += new FileInfo(file).Length;
+        }
+
+        foreach (string subDirectory in Directory.GetDirectories(folderPath))
+        {
+            size += GetFolderSize(subDirectory);
+        }
+
+        return size;
+    }
+}
